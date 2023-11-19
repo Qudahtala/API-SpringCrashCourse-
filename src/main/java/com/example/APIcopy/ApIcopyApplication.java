0c0a802f0e1dd39889f73789dcd82a99350642e6
Tala <talaqudah86@gmail.com>
2023-11-19 23:02:35 +0300

    APIadded

---
+++ src/main/java/com/example/APIcopy/ApIcopyApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIcopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIcopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIcopyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/APIcopy/ApIcopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIcopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIcopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
